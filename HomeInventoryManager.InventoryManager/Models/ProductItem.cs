af0ec341f243168c7effeb5cb2778d6cf0f826dd
Kevin Burgon <buburgo@gmail.com>
2023-01-22 21:18:26 -0700

    Move Recipe model to the main project
    Removes the shared project and adds the Recipe object to the main
    project model.  Also fixes a semantics issue that omnisharp picks up for
    some reason.

--- HomeInventoryManager.InventoryManager/Models/ProductItem.cs
+++ HomeInventoryManager.InventoryManager/Models/ProductItem.cs
@@ -1,10 +1,11 @@
 namespace HomeInventoryManager.InventoryManager.Models
-
-public class ProductItem
 {
-    public int ProductItemId { get; set; } = 0;
+	public class ProductItem
+	{
+		public int ProductItemId { get; set; } = 0;
 
-    public int ProductId { get; set; } = 0;
+		public int ProductId { get; set; } = 0;
 
-    public string ItemBarcodeNumber { get; set; } = string.Empty;
+		public string ItemBarcodeNumber { get; set; } = string.Empty;
+	}
 }
