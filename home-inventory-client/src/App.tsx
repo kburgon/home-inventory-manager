3ae7b8ba1926751fa34ddf5eeff3b02b399079cb
Kevin Burgon <buburgo@gmail.com>
2023-08-28 23:08:46 -0600

    Add more custom form information to client app

--- home-inventory-client/src/App.tsx
+++ home-inventory-client/src/App.tsx
@@ -3,15 +3,17 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-		<Outlet />
-		<div className="footer">
-			<Navbar />
-		</div>
-    </div>
-  );
+class App extends React.Component {
+	public render() {
+		return (
+			<div className="App">
+				<Outlet />
+				<div className="footer">
+					<Navbar />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- home-inventory-client/src/components/ItemTransactionForm.tsx
+++ home-inventory-client/src/components/ItemTransactionForm.tsx
@@ -1,22 +1,44 @@
 import React from 'react';
 
-const ItemTransactionForm = () => {
-	return (
-		<form>
-			<div className="inputRow">
-				<label className="inputLabel" >Item Number: </label>
-				<input type="text" id="itemBarcodeInput" />
-			</div>
-			<div className="inputRow">
-				<label className="inputLabel">Amount: </label>
-				<input type="text" id="transactionAmountInput" />
-			</div>
-			<div className="inputRow">
-				<input type="button" value="Add Items" />
-				<input type="button" value="Remove Items" />
-			</div>
-		</form>
-	);
+class ItemTransactionForm extends React.Component {
+	constructor(props:any) {
+		super(props);
+		this.state = {
+			itemNumber: '',
+			amount: 0.0
+		};
+
+		this.handleInputChange = this.handleInputChange.bind(this);
+	}
+
+	handleInputChange(event:any) {
+		const target = event.target;
+		const value = target.type === 'checkbox' ? target.checked : target.value;
+		const name = target.name;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	render() {
+		return (
+			<form>
+				<div className="inputRow">
+					<label className="inputLabel" >Item Number: </label>
+					<input type="text" id="itemBarcodeInput" />
+				</div>
+				<div className="inputRow">
+					<label className="inputLabel">Amount: </label>
+					<input type="text" id="transactionAmountInput" />
+				</div>
+				<div className="inputRow">
+					<input type="button" value="Add Items" />
+					<input type="button" value="Remove Items" />
+				</div>
+			</form>
+		);
+	}
 }
 
 export default ItemTransactionForm;
