11f53c31b1c582e81c8cd57a9a0a6a1cf7fba0b5
Kevin Burgon <buburgo@gmail.com>
2023-09-03 15:15:50 -0600

    Create working form

--- home-inventory-client/src/components/ItemTransactionForm.tsx
+++ home-inventory-client/src/components/ItemTransactionForm.tsx
@@ -1,29 +1,52 @@
 import { useState } from 'react';
 
 function ItemTransactionForm() {
-	const [inputs, setInputs] = useState({});
+	const [inputs, setInputs] = useState({itemBarcode : "", transactionAmount : 0.0});
 
-	const handleChange = (event) => {
+	const handleChange = (event:any) => {
 		const name = event.target.name;
 		const value = event.target.value;
 		setInputs(values => ({...values, [name]:value}));
 	};
 
+	const handleSubmit = (event:any) => {
+		event.preventDefault();
+		const submitter = event.nativeEvent.submitter.name;
+		if (submitter === "addItems") {
+			console.log("adding items");
+		}
+		else {
+			console.log("removing items");
+		}
+	}
+
 	// Continue following here: https://www.w3schools.com/react/react_forms.asp
 
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className="inputRow">
 				<label className="inputLabel" >Item Number: </label>
-				<input type="text" id="itemBarcodeInput" />
+				<input 
+					type="text" 
+					id="itemBarcodeInput" 
+					name="itemBarcode"
+					value={inputs.itemBarcode || ""}
+					onChange={handleChange}
+				/>
 			</div>
 			<div className="inputRow">
 				<label className="inputLabel">Amount: </label>
-				<input type="text" id="transactionAmountInput" />
+				<input 
+					type="text" 
+					id="transactionAmountInput" 
+					name="transactionAmount"
+					value={inputs.transactionAmount || 0}
+					onChange={handleChange}
+				/>
 			</div>
 			<div className="inputRow">
-				<input type="button" value="Add Items" />
-				<input type="button" value="Remove Items" />
+				<input type="submit" name="addItems" value="Add Items" />
+				<input type="submit" name="removeItems" value="Remove Items" />
 			</div>
 		</form>
 	);
