4885d70297602bb7ccc9cf7b7ce3d5150365f1e8
Kevin Burgon <buburgo@gmail.com>
2023-12-27 23:05:04 -0700

    Add some formatting to inventory item view

--- home-inventory-client/src/components/InventoryItem.tsx
+++ home-inventory-client/src/components/InventoryItem.tsx
@@ -2,9 +2,13 @@ import "./InventoryItem.css";
 
 function InventoryItem() {
 	return (
-		<>
-			Inventory Item
-		</>
+		<div className="inventoryItem">
+			<div className="itemTitle">
+				<div className="productName">Oatmeal</div>
+				<div className="barcode">4wF33225</div>
+			</div>
+			<div className="itemStock">7</div>
+		</div>
 	);
 }
 
