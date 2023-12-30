765a66ae93ebc22d248b0f92305dfe0da0861afc
Kevin Burgon <buburgo@gmail.com>
2023-12-30 00:43:38 -0700

    Align InventoryItem components

--- home-inventory-client/src/components/InventoryItem.tsx
+++ home-inventory-client/src/components/InventoryItem.tsx
@@ -7,7 +7,9 @@ function InventoryItem() {
 				<div className="productName">Oatmeal</div>
 				<div className="barcode">4wF33225</div>
 			</div>
-			<div className="itemStock">7</div>
+			<div className="itemStock">
+				<span className="stockAmount">7</span>
+			</div>
 		</div>
 	);
 }
