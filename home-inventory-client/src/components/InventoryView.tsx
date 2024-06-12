b176bdb1ca43568614dba1a350ec4cf24e9788af
Kevin Burgon <buburgo@gmail.com>
2024-06-12 16:15:34 -0400

    Add sorting to inventory view

--- home-inventory-client/src/components/InventoryView.tsx
+++ home-inventory-client/src/components/InventoryView.tsx
@@ -3,12 +3,14 @@ import InventoryItem from "./InventoryItem";
 import { useState } from 'react';
 
 function InventoryView() {
-	const [items] = useState([
+	const [items, setItems] = useState([
 		{ itemName: "Bread", stock: 1, warningThreshold: 3},
 		{ itemName: "Oatmeal", stock: 2, warningThreshold: 1},
 		{ itemName: "Cereal", stock: 3, warningThreshold: 2},
-		{ itemName: "Milk", stock: 4, warningThreshold: 0}
+		{ itemName: "Milk", stock: 4, warningThreshold: 0},
+		{ itemName: "Eggs", stock: 1, warningThreshold: 4 }
 	]);
+	items.sort((a, b) => a.stock > b.stock ? 1 : -1);
 
 	const [searchText, setSearchText] = useState("");
 
