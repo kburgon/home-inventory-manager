f5b98ea36b59f0f9aaafc95014ef8fe714b22755
Kevin Burgon <buburgo@gmail.com>
2024-06-14 22:06:17 -0600

    Remove example code from Project.cs

--- HomeInventoryManager.Api/Program.cs
+++ HomeInventoryManager.Api/Program.cs
@@ -23,11 +23,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.MapGet("/api/products/all", async (IMediator mediator) =>
 {
 	var query = new GetProductsQuery();
@@ -37,24 +32,4 @@ app.MapGet("/api/products/all", async (IMediator mediator) =>
 .WithName("GetAllProducts")
 .WithOpenApi();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
