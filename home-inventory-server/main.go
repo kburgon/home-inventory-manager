431a1c0f7442fc6b0443be033a284bcb6c685fa5
Kevin Burgon <buburgo@gmail.com>
2024-11-16 08:38:12 -0700

    Fix API config/cors settings

--- home-inventory-server/main.go
+++ home-inventory-server/main.go
@@ -1,34 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 type Product struct {
-	productName 		string	`json:"productName"`
-	count 				int		`json:"count"`
-	warningThreshold 	int		`json:"warningThreshold"`
+	ProductName 		string	`json:"productName"`
+	Count 				int		`json:"count"`
+	WarningThreshold 	int		`json:"warningThreshold"`
+}
+
+var sampleProducts = []Product{
+	{ ProductName: "Bread", Count: 4, WarningThreshold: 1 },
+	{ ProductName: "Eggs", Count: 2, WarningThreshold: 1 },
+	{ ProductName: "Milk", Count: 0, WarningThreshold: 1 },
+}
+
+func handleCORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, accept, origin, CacheControl, X-Requested-With, X-CSRF-Token")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
+func getProducts(c *gin.Context) {
+	c.JSON(http.StatusOK, sampleProducts)
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/api/products/all", func(ctx *gin.Context) {
-		results := []Product {
-			{ productName: "Bread", count: 4, warningThreshold: 1},
-			{ productName: "Eggs", count: 2, warningThreshold: 1 },
-			{ productName: "Milk", count: 0, warningThreshold: 1 },
-		}
+	router.Use(handleCORS())
+	router.GET("/api/products/all", getProducts)
 
-		if err := ctx.BindJSON(&results); err != nil {
-			fmt.Printf("ERROR: %s\n", err)
-			ctx.AbortWithError(http.StatusBadRequest, err)
-		}
-
-		ctx.IndentedJSON(http.StatusOK, &results)
-	})
-
-	router.Run(":5223")
+	router.Run("localhost:5223")
 }
